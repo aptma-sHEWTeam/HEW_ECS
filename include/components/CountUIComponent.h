dadb84f489d7dfbdca26cfc7f3bd6a129c9256dd
亀多彩日 NHAT12D19209 <nhs40399@nhs.hal.ac.jp>
2025-11-18 12:39:03 +0900

    CountUIの追加

--- include/components/CountUIComponent.h
+++ include/components/CountUIComponent.h
@@ -5,10 +5,14 @@
  * \author 亀多彩日
  * \date   11/18
  * 
- * 
+ * 3.2.1.Go　描画→削除　カウント時プレイヤー等の動きを停止
  *********************************************************************/
+
 #pragma once
 #include "pch.h"
 #include "scenes/Game.h"
 #include "systems/UISystem.h"
 #include "graphics/TextSystem.h"
+#include <DirectXMath.h>
+#include <string>
+#include <functional>
